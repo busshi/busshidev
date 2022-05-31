90ced06862750cd8a49eeee87f22a5e645be6de7
busshi <busshi973@icloud.com>
2022-05-31 16:40:31 +0200

    add favicon

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from "next";
+import Meta from "../components/Meta";
+import Image from "next/image";
 
 const Home: NextPage = () => {
-  return <div>BUSSHIDEV</div>;
+  return (
+    <div>
+      <Meta />
+      <Image src="/images/logo.svg" alt="busshiDev" layout="fill" />
+    </div>
+  );
 };
 
 export default Home;
