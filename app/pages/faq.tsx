bd957b13584b0421ac5154e37c559d3611422baa
busshi <busshi973@icloud.com>
2022-06-02 15:22:14 +0200

    testing faq jsonld

---
+++ app/pages/faq.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import MetaFaq from "../components/MetaFaq";
+
+const Faq: NextPage = () => {
+  return (
+    <div>
+      <MetaFaq />
+      <div>Who am I?</div>
+      <div>I am busshidev</div>
+      <div>Phone</div>
+      <div>06 12 34 56 78</div>
+    </div>
+  );
+};
+
+export default Faq;
