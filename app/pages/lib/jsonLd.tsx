4e3dc40510efbfa0b0f0e084c3f1aeb09327ceb3
busshi <busshi973@icloud.com>
2022-12-15 17:23:45 +0100

    add website jsonLd

--- app/pages/lib/jsonLd.tsx
+++ app/pages/lib/jsonLd.tsx
@@ -1,6 +1,22 @@
 import { SITE_URL } from "./constants";
 
-export const jsonldLogo = {
+export const jsonLdWebsite = {
+  "@context": "http://schema.org",
+  "@type": "WebSite",
+  name: "busshiDev",
+  alternateName: "BusshiDev - Design. Develop. Deploy. Boost",
+
+  url: `${SITE_URL}`,
+  logo: `${SITE_URL}/logo.png`,
+  sameAs: [
+    "https://www.malt.fr/profile/alexandredubar",
+    "https://www.linkedin.com/in/alexandre-dubar/",
+    "https://github.com/busshi",
+    "https://busshi.fr",
+  ],
+};
+
+export const jsonLdLogo = {
   "@context": "https://schema.org",
   "@type": "Organization",
   name: "busshiDev",
