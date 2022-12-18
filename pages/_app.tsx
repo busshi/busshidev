ef74b13df68c7f002d7ca4ebca15eaba8be9c60a
busshi <busshi973@icloud.com>
2022-12-18 10:38:12 +0100

    add _document.tsx to fix css render on server side instead of client side

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from "next/app";
 import { HighlightedColorProvider } from "../providers/HighlightedColor";
+import Metadata from "./components/Metadata";
 import "./styles/app.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <HighlightedColorProvider>
+      <Metadata />
       <Component {...pageProps} />
     </HighlightedColorProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
 import styled from "styled-components";
 import { Body } from "./components/Body";
 import { Footer } from "./components/Footer";
-import { Metadata } from "./components/Metadata";
 import { TopBar } from "./components/TopBar";
 
 export default function Home() {
   return (
     <App>
-      <Metadata />
       <TopBar />
       <Body />
       <Footer />
