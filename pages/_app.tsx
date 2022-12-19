a15ae5a4e05be7090ac8d788a2c7fa12828aa33a
busshi <busshi973@icloud.com>
2022-12-19 02:59:02 +0100

    google tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,11 @@ import "./styles/app.css";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <HighlightedColorProvider>
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=G-K4ERH99TW7" height="0" width="0" style="display: none; visibility: hidden;" />`,
+        }}
+      />
       <Component {...pageProps} />
     </HighlightedColorProvider>
   );
