2b814a0197a530cfc72f028b107b770a9dec4383
busshi <busshi973@icloud.com>
2023-01-05 19:08:41 +0100

    fix globe resizing

--- components/Globe.tsx
+++ components/Globe.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { useRef } from "react";
 import world from "../lib/world.json";
 import { useIsDarkMode } from "../hooks/useIsDarkMode";
+import { useWindowSize } from "../hooks/useWindowSize";
 
 let Globe = () => null;
 if (typeof window !== "undefined") Globe = require("react-globe.gl").default;
@@ -10,19 +11,33 @@ if (typeof window !== "undefined") Globe = require("react-globe.gl").default;
 const SpinningGlobe = () => {
   const isDarkMode = useIsDarkMode();
   const ref = useRef();
+  const size = useWindowSize();
 
   return (
     <Container>
       <Globe
         //@ts-ignore
         showGlobe={false}
+        width={size.width}
+        height={size.height}
         backgroundColor={isDarkMode ? "#121212" : "#f1f1f1"}
         ref={ref}
         hexPolygonsData={world}
         hexPolygonMargin={0.7}
+        //  hexPolygonColor={() => (isDarkMode ? "" : "#3a228a")}
         onGlobeReady={() => {
-          //@ts-ignore
-          if (ref.current) ref.current.controls().autoRotate = true;
+          if (ref.current) {
+            //@ts-ignore
+            ref.current.controls().autoRotate = true;
+            //@ts-ignore
+            ref.current.controls().minDistance = 200;
+            //@ts-ignore
+            ref.current.controls().maxDistance = 500;
+            //@ts-ignore
+            ref.current.controls().minPolarAngle = Math.PI / 3.5;
+            //@ts-ignore
+            ref.current.controls().maxPolarAngle = Math.PI - Math.PI / 3;
+          }
         }}
         showAtmosphere={true}
         atmosphereColor={isDarkMode ? "#3a228a" : "#444444"}
