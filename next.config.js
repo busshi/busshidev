eecd6ea2354a2d140acfd7d19d30f259b7378071
busshi <busshi973@icloud.com>
2023-02-17 17:06:54 +0100

    i18n next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
